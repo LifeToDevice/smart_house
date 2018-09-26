e2c6cb2b2f8f6c8e4398e5de5368e5454c9fb01a
LifeToDevice <dx8vb@mail.ru>
2018-09-26 12:28:08 +0600

    Update hc165.h
    Fix comments codepage

--- firmware/exec_unit/hc165.h
+++ firmware/exec_unit/hc165.h
@@ -3,7 +3,7 @@
 
 #include "../common/types.h"
 
-// Инициализация HC165
+// РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ HC165
 void hc165_init( )
 {
     HC165_LOAD_DIR  = 0;
@@ -14,8 +14,8 @@ void hc165_init( )
     HC165_CLOCK_PIN = 0;
 }
 
-// Чтения состояния HC165 
-// bits_count - количество бит ( одна HC165 - 8 бит, две - 16, три - 24, четыре - 32)
+// Р§С‚РµРЅРёРµ HC165 
+// bits_count - РєРѕР»РёС‡РµСЃС‚РІРѕ Р±РёС‚ ( РѕРґРЅР° HC165 - 8 Р±РёС‚, РґРІРµ - 16, С‚СЂРё - 24, С‡РµС‚С‹СЂРµ - 32)
 ulong hc165_read( uchar bits_count )
 {
     unsigned char i;
@@ -28,7 +28,7 @@ ulong hc165_read( uchar bits_count )
     for ( i = 0; i < bits_count; i++ )
     {
         val <<= 1;
-        if ( HC165_DATA_PIN == 0 ) // Инвертировано
+        if ( HC165_DATA_PIN == 0 )
             val |= 1;
 
         HC165_CLOCK_PIN = 0;
